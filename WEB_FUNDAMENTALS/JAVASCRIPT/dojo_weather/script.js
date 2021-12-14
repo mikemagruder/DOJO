834fc5286202d8bbd48309427ac31b3c7546a815
Mike Magruder <mike_magruder@yahoo.com>
2021-12-14 15:57:15 -0800

    onChange assignment

--- WEB_FUNDAMENTALS/JAVASCRIPT/dojo_weather/script.js
+++ WEB_FUNDAMENTALS/JAVASCRIPT/dojo_weather/script.js
@@ -10,7 +10,9 @@ function convert(element) {
     console.log(element.value);
     for(var i=1; i<9; i++) {
         var tempSpan = document.querySelector("#temp" + i);
+        console.log(tempSpan);
         var tempVal = parseInt(tempSpan.innerText);
+        console.log(tempVal)
         if(element.value == "°C") {
             tempSpan.innerText = f2c(tempVal);
         } else {

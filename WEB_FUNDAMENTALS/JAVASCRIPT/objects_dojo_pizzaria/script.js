12a4a2b0c0d91a8e5257e9704dbdad9f2a51adb8
Mike Magruder <mike_magruder@yahoo.com>
2021-12-15 09:13:50 -0800

    Objects Challenge

--- WEB_FUNDAMENTALS/JAVASCRIPT/objects_dojo_pizzaria/script.js
+++ WEB_FUNDAMENTALS/JAVASCRIPT/objects_dojo_pizzaria/script.js
@@ -26,3 +26,71 @@ var p3 = pizzaOven("regular", "marinara", "cheddar", ["mushrooms", "olives", "on
 console.log(p3);
 var p4 = pizzaOven("hand tossed", "pesto", ["mozzarella", "feta"], ["sausage", "spinach", "onions"]);
 console.log(p4);
+
+var crustTypes = [
+    "deep dish",
+    "hand tossed",
+    "thin and crispy",
+    "cauliflower",
+    "gluten free",
+    "hawaiian bread"
+];
+
+var sauceTypes = [
+    "traditional",
+    "marinara",
+    "bbq",
+    "white sauce",
+    "nacho cheese",
+    "tikka masala"
+];
+
+var cheeses = [
+    "mozzarella",
+    "cheddar",
+    "feta",
+    "swiss cheese",
+    "blue cheese",
+    "goat cheese",
+    "provolone",
+    "parmesan",
+    "vegan cheese"
+];
+
+var toppings = [
+    "pepperoni",
+    "sausage",
+    "chicken",
+    "corn",
+    "olives",
+    "bell peppers",
+    "onions",
+    "mushrooms",
+    "anchovies"
+];
+
+function randomRange(max, min) {
+    return Math.floor(Math.random() * max - min) + min;
+}
+
+function randomPick(arr) {
+    var i = Math.floor(arr.length * Math.random());
+    return arr[i];
+}
+
+function randomPizza() {
+    var pizza = {};
+    pizza.crustType = randomPick(crustTypes);
+    pizza.sauceType = randomPick(sauceTypes);
+    pizza.cheeses = [];
+    pizza.toppings = [];
+    for(var i=0; i<randomRange(5, 1); i++) {
+        pizza.cheeses.push(randomPick(cheeses));
+    }
+    for(var i=0; i<randomRange(4, 0); i++) {
+        pizza.toppings.push(randomPick(toppings));
+    }
+    return pizza;
+}
+
+console.log(randomPizza());
